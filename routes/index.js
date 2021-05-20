a6c133f9b6e615a6eb1625839f613b5d38501382
UnnamedXAer <UnnamedXAer@gmail.com>
2021-05-20 13:01:04 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const startDate = new Date();
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: ' I will try to host my express app on firebase.' });
+router.get('/', function (req, res, next) {
+	res.render('index', {
+		serverStartAt: startDate.toUTCString(),
+		loadedAt: new Date().toUTCString()
+	});
 });
 
 module.exports = router;
